f7e323d364d43d35c4108e32a4cd76172d2cce88
Thanh Tung <nagisakun1019@gmail.com>
2024-07-21 20:37:14 +0700

    add Admin folder

---
+++ ProjectPRN_RestaurantManagement/Pages/Admin/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectPRN_RestaurantManagement.Pages.Admin
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ProjectPRN_RestaurantManagement/obj/Debug/net6.0/ProjectPRN_RestaurantManagement.AssemblyInfo.cs
+++ ProjectPRN_RestaurantManagement/obj/Debug/net6.0/ProjectPRN_RestaurantManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPRN_RestaurantManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a0cca48e0319fc44e768f97f6e44886ba11edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPRN_RestaurantManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPRN_RestaurantManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
